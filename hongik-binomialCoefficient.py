dcd387a64ddeef818044b2a509e68768043285c2
김기윤 <gimgiyun@gimgiyun-ui-MacBookPro.local>
2021-05-31 21:42:24 +0900

    [FIX] dp - binomial coefiiceint

--- hongik-binomialCoefficient.py
+++ hongik-binomialCoefficient.py
@@ -2,16 +2,18 @@ n = int(input())
 k = int(input())
 
 dp = [[0 for j in range(0, k+1)] for i in range(0, n+1)]
-
-print(dp)
  
 
 def b_coef(n, k):
     
     for i in range(0, n+1):
-        for j in range(0, min(i, k)):
-            if k==0 or n==k:
-                dp[n][k] = 1
+        for j in range(0, min(i, k)+1):
+            if j==0 or j==i:
+                dp[i][j] = 1
             else:
-                dp[i][j] = dp[i-1][k-1] + dp[i-1][k]
+                dp[i][j] = dp[i-1][j-1] + dp[i-1][j]
     return dp[n][k]
+
+print(b_coef(n, k))
+print(dp)
+
