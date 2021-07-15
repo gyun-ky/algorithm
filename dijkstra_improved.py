b3eb0c17e24425f8150279e31d9f12128f70b232
kim_ki_yun <kiyoon9390@gmail.com>
2021-07-15 23:36:05 +0900

    [FIX] dijkstra_improved

--- dijkstra_improved.py
+++ dijkstra_improved.py
@@ -13,6 +13,36 @@ visited = [False]*(n+1)
 
 distance = [INF]*(n+1)
 
+for i in range(0, m):
+    a, b, c = map(int, input().split())
+    graph[a].apppend((b, c))
+
+
+
+def dijkstra(start):
+    q = []
+    distance[start] = 0
+    heapq.heappush(q, (0, start))
+
+    while q :
+        dist, now = heapq.heappop(q)
+        if distance[now] < dist:
+            continue
+        for i in graph[now]:
+            cost = dist + i[1]
+            if cost < distance[i[0]]:
+                distance[i[0]] = cost
+                heapq.heappush(q, (cost, i[0]))
+
+dijkstra(start)
+
+for i in range(1, n+1):
+    if distance[i] == INF:
+        print("INFINITY")
+    else:
+        print(distance[i])
+
+
 
 
 
