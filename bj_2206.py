10368df9a52225d6f38b00dc28a03145d29059c1
kim_ki_yun <kiyoon9390@gmail.com>
2021-11-21 20:02:21 +0900

    [FEAT] bj_2206 complete

--- bj_2206.py
+++ bj_2206.py
@@ -1,20 +1,22 @@
 from collections import deque
 import sys
 input = sys.stdin.readline
+max_num = int(1e9)
 
 N, M = map(int, input().split())
 
-crash_visited = [[False]*M]*N
-not_crash_visited = [[False]*M]*N
+visited = [[[max_num for col in range(M)] for row in range(N)] for depth in range(2)]
+# visited[row][col][0] : 벽 1회 깨기 전의 방문 최소 cnt 횟수
+# visited[row][col][1] : 벽 1회 꺠기 후의 방문 최소 cnt 횟수
+
 
 data = []
 
 for _ in range(N):
     data.append(list(map(int, input().strip())))
 
-print(data)
-print(crash_visited)
-print(not_crash_visited)
+
+
 
 dy = [0, 1, 0, -1]
 dx = [1, 0, -1, 0]
@@ -23,14 +25,11 @@ dx = [1, 0, -1, 0]
 # (0, 0)부터 시작해서 (N-1, M-1) 까지 가는 것으로
 q = deque()
 
-min_cnt = int(1e9)
 
 def bfs(col, row, cnt):
-    global min_cnt
 
     q.append((col, row, cnt, False))
-    not_crash_visited[col][row] = True
-    crash_visited[col][row] = True
+    visited[0][col][row] = cnt
 
     while q:
 
@@ -44,25 +43,40 @@ def bfs(col, row, cnt):
                 continue
 
             if ny == N-1 and nx == M-1:
-                print("여기 왔다")
-                min_cnt = min(min_cnt, cur[2])
+                if cur[3] == False:
+                    visited[0][ny][nx] = min(visited[0][ny][nx], cur[2]+1)
+                else:
+                    visited[1][ny][nx]= min(visited[1][ny][nx], cur[2]+1)
+
                 continue
 
-            if data[ny][nx] == 0:
-                
-                if cur[3] == False and not_crash_visited[ny][nx] == False:
-                    q.append((ny, nx, cur[2]+1, False))
-                    not_crash_visited[ny][nx] = True
-
-                elif cur[3] == True and crash_visited[ny][nx] == False:
-                    q.append((ny, nx, cur[2]+1, True))
-                    crash_visited[ny][nx] = True
-
-            if data[ny][nx] == 1 and cur[3] == False:
-                q.append((ny, nx, cur[2]+1, True))
             
+            # 1번 벽 깨기를 하지 않은 경우
+            if cur[3] == False:
+                # 1번도 벽깨기 안한 경우, 벽을 만나면 방문하지 않은 벽일 경우 깨기
+                if data[ny][nx] == 1 and visited[1][ny][nx] == int(1e9):
+                    visited[1][ny][nx] = cur[2]+1
+                    q.append((ny, nx, cur[2]+1, True))
+                
+                # 1번도 벽깨기 안한 경우, 빈칸을 만나면 방문하지 않은 경우 진입
+                if data[ny][nx] == 0 and visited[0][ny][nx] == int(1e9):
+                    visited[0][ny][nx] = cur[2]+1
+                    q.append((ny, nx, cur[2]+1, False))
 
-bfs(0, 0, 1)
+            # 1번 이상 벽깨기를 한 후인 경우
+            else:
+                # 1번 이상 벽꺠기를 한 경우, 빈칸을 만나면 진행
+                if data[ny][nx] == 0 and visited[1][ny][nx] == int(1e9):
+                    visited[1][ny][nx] = cur[2]+1
+                    q.append((ny, nx, cur[2]+1, True))
 
-if min_cnt == int(1e9):
+                # 1번이상 벽깨기 한 경우, 빈칸 아닌거 만나면 진행 X
+
+
+    return min(visited[0][N-1][M-1], visited[1][N-1][M-1])
+
+result = bfs(0, 0, 1)
+if result == int(1e9):
     print(-1)
+else:
+    print(result)
