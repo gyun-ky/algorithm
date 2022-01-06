ca6134a64c3bced664818558c9b4454470e3151a
kim_ki_yun <kiyoon9390@gmail.com>
2022-01-07 00:23:07 +0900

    [FEAT] bj_16236

--- bj_16236.py
+++ bj_16236.py
@@ -11,7 +11,8 @@ for i in range(N):
     for j in range(N):
         if data[i][j] == 9:
             data[i][j] = 0
-            shark_row, shark_col = i, j
+            shark_row = i
+            shark_col = j
 
 dy = [-1, 0, 0, 1]
 dx = [0, -1, 1, 0]
@@ -20,7 +21,7 @@ def bfs_eating(shark_row, shark_col, cur_size):
     q = deque()
     q.append((shark_row, shark_col, cur_size))
     
-    visited = [[-1 for c in range(N)]for r in range(N)]
+    visited = [[-1] * N for r in range(N)]
     visited[shark_row][shark_col] = 0
     eatable = []
 
@@ -30,14 +31,16 @@ def bfs_eating(shark_row, shark_col, cur_size):
         for i in range(4):
             ny = y + dy[i]
             nx = x + dx[i]
-
+    
             if 0<=ny<N and 0<=nx<N and visited[ny][nx] == -1:
                 if data[ny][nx] == size or data[ny][nx] == 0:
                     visited[ny][nx] = visited[y][x] + 1
+                    
                     q.append((ny, nx, size))
 
                 elif data[ny][nx] < size:
                     visited[ny][nx] = visited[y][x] + 1
+                    q.append((ny, nx, size))
                     eatable.append((visited[ny][nx], ny, nx, size))
 
     if len(eatable) > 0:
@@ -53,7 +56,7 @@ shark_size = 2
 go = True
 while go:
     for i in range(shark_size):
-        time, s_row, s_col, s_size = bfs_eating(shark_col, shark_row, shark_size)
+        time, s_row, s_col, s_size = bfs_eating(shark_row, shark_col, shark_size)
 
         if time == -1 and s_col == -1 and s_row == -1 and s_size == -1:
             go = False
@@ -61,6 +64,7 @@ while go:
         data[s_row][s_col] = 0
         answer += time
         shark_col, shark_row = s_col, s_row
+    shark_size += 1
 
 
 print(answer)
