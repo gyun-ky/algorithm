ac0e31e654d43d6f01198b2332e18d6f75801566
kim_ki_yun <kiyoon9390@gmail.com>
2022-01-16 22:15:40 +0900

    [FEAT] bj_16989

--- bj_15989.py
+++ bj_15989.py
@@ -1,3 +1,19 @@
 import sys
-input = sys.stdin.readline()
+# input = sys.stdin.readline()
+
+n = int(input())
+
+dp = [1] * 10001
+
+for i in range(2, 4):
+    for j in range(1, 10001):
+        if j >= i:
+            dp[j] += dp[j-i]
+
+
+        
+
+for i in range(n):
+    a = int(input())
+    print(dp[a])
 
