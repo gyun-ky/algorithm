346457da21930c550f91ff45e6ec26c9ce0493c5
kim_ki_yun <kiyoon9390@gmail.com>
2022-01-25 00:26:09 +0900

    [FEAT] bj_16234

--- bj_16234.py
+++ bj_16234.py
@@ -1,2 +1,61 @@
+from collections import deque
 
-import sys
+N, L, R = map(int, input().split())
+
+A = []
+for _ in range(N):
+    A.append(list(map(int, input().split())))
+
+dy = [0, 1, 0, -1]
+dx = [1, 0, -1, 0]
+
+day = 0
+while True:
+
+    move = False
+    visited = [[False]*N for _ in range(N)]
+    total_visit = N*N
+
+    for r in range(N):
+        for c in range(N):
+            if visited[r][c] == True:
+                continue
+            q = deque()
+            q.append((r,c))
+            united = [(r, c)]
+            people_sum = A[r][c]
+            visited[r][c] = True
+            total_visit -= 1
+
+            while q:
+                y, x = q.popleft()
+
+                for i in range(4):
+                    ny = y + dy[i]
+                    nx = x + dx[i]
+
+                    if 0<= ny < N and 0 <= nx < N and visited[ny][nx] == False:
+                        if L<= abs(A[ny][nx] - A[x][y]) <= R:
+                            q.append((ny, nx))
+                            united.append((ny, nx))
+                            people_sum += A[ny][nx]
+                            visited[ny][nx] = True
+                            total_visit -=1
+
+
+            if len(united) == 1:
+                continue
+            else:
+                move = True
+                after_cnt = int(people_sum/len(united))
+                for n in united:
+                    A[n[0]][n[1]] = after_cnt
+            
+        if total_visit == 0:
+            break
+
+    if move is True:
+        day+=1
+    else:
+        print(day)
+        break
