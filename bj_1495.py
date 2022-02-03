13ebc03b3a62c569bc3bb552739a7145004a96e5
kim_ki_yun <kiyoon9390@gmail.com>
2022-02-03 23:32:03 +0900

    [FEAT] bj_1495 2rd

--- bj_1495.py
+++ bj_1495.py
@@ -3,49 +3,28 @@ N, S, M = map(int, input().split())
 V = list(map(int, input().split()))
 V.insert(0, -1)
 print(V)
-dp = [[0]*(N+1) for i in range(2)]
+dp = [[False]*M for i in range(N)]
 print(dp)
 
-dp[0][0] = S
-dp[1][0] = S
 
-for j in range(1, N+1):
+if 0<= S-V[0] <M:
+    dp[0][S-V[0]] = True
 
-    if dp[0][j-1] == -1:
-        a = -1
-    else:
-        a = dp[0][j-1] - V[j]
-        if a < 0 or a > M:
-            a = -1
-    
-    if dp[1][j-1] == -1:
-        b = -1
-    else:
-        b = dp[1][j-1] - V[j]
-        if b < 0 or b > M:
-            b = -1
-    
-    dp[0][j] = max(a, b)
-    
-    if dp[0][j-1] == -1 :
-        c = -1
-    else:
-        c = dp[0][j-1] + V[j]
-        if c < 0 or c > M:
-            c = -1
+if 0<= S+V[0] <M:
+    dp[0][S+V[0]] = True
 
-    if dp[1][j-1] == -1:
-        d = -1
-    else: 
-        d = dp[1][j-1] + V[j]
-        if d < 0 or d > M:
-            d = -1
-    
-    dp[1][j] = max(c, d)
-    
-for i in range(2):
-    print(dp[i])
+answer = 0
+for i in range(1, N):
+    for j in range(0, M):
+        if 0<= S-V[0] <M and dp[i-1][S+V[0]]:
+            dp[i][S-V[0]] = True
+            if i== N-1:
+                answer = max(answer, j)
 
-print(max(dp[1][N], dp[0][N]))
+        if 0<= S+V[0] <M and dp[i-1][S-V[0]]:
+            dp[i][S+V[0]] = True
+            if i== N-1:
+                answer = max(answer, j)
 
 
+print(answer)
