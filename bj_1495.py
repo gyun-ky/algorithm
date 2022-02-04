a75a78c8e1f9f0c792268f2f4cf2523ba66391cc
kim_ki_yun <kiyoon9390@gmail.com>
2022-02-04 11:20:56 +0900

    [FIX] bj_1495

--- bj_1495.py
+++ bj_1495.py
@@ -1,30 +1,33 @@
 N, S, M = map(int, input().split())
 
 V = list(map(int, input().split()))
-V.insert(0, -1)
-print(V)
-dp = [[False]*M for i in range(N)]
-print(dp)
 
+dp = [[False]*(M+1) for i in range(N)]
 
-if 0<= S-V[0] <M:
+answer = -1
+
+if 0<= S-V[0] <=M:
     dp[0][S-V[0]] = True
+    if N-1 == 0:
+        answer = max(answer, S-V[0])
 
-if 0<= S+V[0] <M:
+if 0<= S+V[0] <=M:
     dp[0][S+V[0]] = True
+    if N-1 == 0:
+        answer = max(answer, S+V[0])
 
-answer = 0
+
 for i in range(1, N):
-    for j in range(0, M):
-        if 0<= S-V[0] <M and dp[i-1][S+V[0]]:
-            dp[i][S-V[0]] = True
+    for j in range(0, M+1):
+        
+        if 0<= j-V[i] <=M and dp[i-1][j-V[i]]:
+            dp[i][j] = True
             if i== N-1:
                 answer = max(answer, j)
 
-        if 0<= S+V[0] <M and dp[i-1][S-V[0]]:
-            dp[i][S+V[0]] = True
+        if 0<= j+V[i] <=M and dp[i-1][j+V[i]]:
+            dp[i][j] = True
             if i== N-1:
                 answer = max(answer, j)
-
 
 print(answer)
