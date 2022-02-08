54b3bb437f46ebd40d006bba61fd196522fb9ed3
kim_ki_yun <kiyoon9390@gmail.com>
2022-02-08 17:32:52 +0900

    fix:bj_16235

--- bj_16235.py
+++ bj_16235.py
@@ -9,7 +9,7 @@ for i in range(N):
     A.append(list(map(int, input().split())))
     nut.append(A[i])
 
-
+print(nut)
 trees = [[] for _ in range(N*N)]
 
 for _ in range(M):
@@ -21,27 +21,45 @@ adj = [
 (-1, -1), (-1, 0), (-1, 1), (0, -1), (0, 1), (1, -1), (1, 0), (1, 1) 
 ]
 
+print(trees)
+
 while K:
     
     for i in range(N):
         for j in range(N):
             section_idx = N*i + j
             trees_len = len(trees[section_idx])
-            if  trees_len > 1:
+            if  trees_len >= 1:
                 trees[section_idx].sort()
             
-            rm_start_idx = trees_len-1
-            for t_idx in range(trees_len):
-                if nut[i][j] < trees[section_idx][t_idx]:
-                    rm_start_idx = t_idx
-                    break
-                nut[i][j] -= trees[section_idx][t_idx]
-                trees[section_idx][t_idx] += 1
+                rm_start_idx = trees_len
+                end = False
 
-            for _ in range(rm_start_idx, trees_len):
-                nut[i][j] += (trees[section_idx][rm_start_idx] // 2)
-                del trees[section_idx][rm_start_idx]
+                idx = 0
+                rm_start_idx = -1
+                rm_flag = False
+                
+                print(trees[section_idx])
 
+                while idx < trees_len:
+                    if rm_flag is False:
+                        if nut[i][j] < trees[section_idx][idx]:
+                            rm_flag = True
+                            rm_start_idx = idx
+                        else:
+                            nut[i][j] -= trees[section_idx][idx]
+                            trees[section_idx][idx] += 1
+                    
+                    if rm_flag is True:
+                        nut[i][j] += (trees[section_idx][idx] // 2)
+
+                    idx+=1
+                
+                if rm_flag is True:
+                    trees[section_idx] = trees[section_idx][rm_start_idx : trees_len]
+                
+                print(trees[section_idx])
+              
     for i in range(N):
         for j in range(N):
             section_idx = N*i + j
@@ -59,6 +77,8 @@ while K:
 
     K-=1
 
+
+print(trees)
 answer = 0
 for section in trees:
     answer += len(section)
