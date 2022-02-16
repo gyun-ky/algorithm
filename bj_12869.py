97f71ff9998b9c0d7bd4fd78ad933440afad484a
kim_ki_yun <kiyoon9390@gmail.com>
2022-02-16 13:10:46 +0900

    feat:bj_12869

--- bj_12869.py
+++ bj_12869.py
@@ -1,89 +1,53 @@
-# 수빈 뮤탈 1개 강호 SCV N개
-# SCV는 각각의 체력
-
-# 뮤탈 한번에 세개 SCV 공격 9 -> 3 -> 1
-# 한번의 공격에서 같은 SCV 여러번 공격 불가
-
-# 모든 SCV 파괴하기 위한 공격 횟수 최솟값
-from cgitb import scanvars
 from itertools import permutations
-
-
 
 N = int(input())
 
-attack = []
-for i in range(N):
-    attack.append(i)
-
+attack = [-9, -3, -1]
 
 hp = list(map(int, input().split()))
-print(hp)
 
-perm = list(permutations(attack, N))
-print(perm)
+# 공격 가능한 순열 리스트
+perm = list(permutations(attack, 3))
 
-# dp는 SCV의 체력이 i 일 떄, 최소 횟수
+dp = [[[-1 for k in range(61)] for j in range(61)] for i in range(61)]
 
-# 1회 진행했을 때, 남아있는 hp?
-
-# 배열에다가 각각의 체력일 때, 1, 2, 3 공격을 적용 했을 때를 모두 구한다
-#    0, 1, 2, 4, 5, ... hp
-# 0
-# 1
-# 2
-scv_dp_hp = []
-minus_hp = [9, 3, 1]
-for i in range(N):
-    dp = []
-    for k in range(3):
-        row = []
-        for j in range(hp[i]+1):
-            if j-minus_hp[k] <= 0:
-                row.append(0)
-            else:
-                row.append(j-minus_hp[k])
-        dp.append(row)
-    # dp = [[j-minus_hp[k] for j in range(hp[i])] for k in range(3)]
-    print(dp)
-    scv_dp_hp.append(dp)
-
-print(scv_dp_hp[0][2][4])
+def dp_cal(i, j, k):
+    if i<=0 and j<=0 and k<=0 :
+        return 0
 
 
-# 이런 배열이 3개
-# hp 부터 시작해서 각 경우 일때의  최소 hp로 시작 그 수를 센다.
-cnt = 0
-tot_hp = sum(hp)
-print(tot_hp)
-print("-------")
-while tot_hp:
-    fix_perm = []
+    if i<0:
+        ni = 0
+    else:
+        ni = i
+    if j<0:
+        nj = 0
+    else: 
+        nj = j
+    if k<0:
+        nk = 0
+    else: 
+        nk = k
+
+    if dp[ni][nj][nk] != -1:
+        return dp[ni][nj][nk]
+
+    min_num = int(1e9)
     for p in perm:
-        n_tot_hp = 0
-
-        for scv_idx in range(N):
-            n_tot_hp += scv_dp_hp[scv_idx][p[scv_idx]][hp[scv_idx]]
-            
-
-        if tot_hp > n_tot_hp :
-            fix_perm = p
-
+        result = dp_cal(ni+p[0], nj+p[1], nk+p[2]) + 1
+        if result < min_num:
+            min_num = result
     
-    for scv_idx in range(N):
-        n_tot_hp += scv_dp_hp[scv_idx][fix_perm[scv_idx]][hp[scv_idx]]
-        hp[scv_idx] = scv_dp_hp[scv_idx][fix_perm[scv_idx]][hp[scv_idx]]
+    dp[ni][nj][nk] = min_num
+    return min_num
 
-    tot_hp = n_tot_hp
-    
-    
-    print(hp)
-    
-    cnt += 1
-    
-# 전체가 작으면 최소라고 생각했는데 아닌가 보다....! 다시 생각...!
+if N==1:
+    print(dp_cal(hp[0], 0, 0))
+elif N==2:
+    print(dp_cal(hp[0], hp[1], 0))
+else:
+    print(dp_cal(hp[0], hp[1], hp[2]))
 
-print(cnt)
 
 
 
