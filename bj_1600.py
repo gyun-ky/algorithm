912678c3299f42e7df0b34d7f81d5d72c3f7f92b
kim_ki_yun <kiyoon9390@gmail.com>
2022-03-09 00:27:47 +0900

    feat:bj_1600

--- bj_1600.py
+++ bj_1600.py
@@ -30,41 +30,49 @@ dy = [1, 0, -1, 0]
 # (row 좌표, col 좌표, 횟수, 말처럼이동가능 남은 횟수)
 
 def bfs():
-    visited = [[-1]*W for _ in range(H)]
-    q = deque((0, 0, 0, K))
-    visited[0][0] = 0
+    visited = [[[-1]*W for _ in range(H)]for k in range(K+1)]
+    q = deque()
+    q.append((0, 0, 0, K))
+    visited[K][0][0] = 0
 
+    ans = int(1e9)
     while q:
         row, col, cnt, horse_move = q.popleft()
 
         if row == H-1 and col == W-1:
+            ans = min(ans, cnt)
             continue
+
+        # 말처럼 움직일 수 있을 때
+        if horse_move > 0:
+            for d in range(8):
+                nrow = row + horse_dx[d]
+                ncol = col + horse_dy[d]
+
+                if nrow<0 or nrow>=H or ncol<0 or ncol>=W or visited[horse_move-1][nrow][ncol] != -1 or grid[nrow][ncol] == 1:
+                    continue
+                
+                q.append((nrow, ncol, cnt+1, horse_move-1))
+                visited[horse_move-1][nrow][ncol] = cnt+1
+
 
         # 원숭이 move 일 때,
         for d in range(4):
             nrow = row + dx[d]
             ncol = col + dy[d]
 
-            if nrow<0 or nrow>=H or ncol<0 or ncol>=W or visited[nrow][ncol] != -1 and grid[nrow][ncol] == 1:
+            if nrow<0 or nrow>=H or ncol<0 or ncol>=W or visited[horse_move][nrow][ncol] != -1 or grid[nrow][ncol] == 1:
                 continue
 
             q.append((nrow, ncol, cnt+1, horse_move))
-            visited[nrow][ncol] = cnt+1
+            visited[horse_move][nrow][ncol] = cnt+1
 
 
-        if horse_move > 0:
-            for d in range(8):
-                nrow = row + dx[d]
-                ncol = col + dy[d]
-
-                if nrow<0 or nrow>=H or ncol<0 or ncol>=W :
-                    continue
-
-                if visited[nrow][ncol] == -1 or visited[nrow][ncol] > (cnt+1):
-                    q.append((nrow, ncol, cnt+1, horse_move-1))
-                    visited[nrow][ncol] = cnt+1
-
-    return visited[H-1][W-1]
+        
+    if ans == int(1e9):
+        return -1
+    else:
+        return ans
 
 print(bfs())
 
--- test1.py
+++ test1.py
@@ -1,45 +1,58 @@
-from itertools import permutations
-from operator import itemgetter, attrgetter
+# from itertools import permutations
+# from operator import itemgetter, attrgetter
 
-a = [(1,2), (2, 2), (2, 1)]
-test = [1, 3, 2]
-a.sort(key = itemgetter(0, 1))
+# a = [(1,2), (2, 2), (2, 1)]
+# test = [1, 3, 2]
+# a.sort(key = itemgetter(0, 1))
+# print(a)
+
+
+# result = sorted(a, key= lambda a : (a[0], a[1]))
+# print(result)
+
+# class User:
+    
+#     def __init__(self, id, pwd):
+#         self.id = id
+#         self.pwd = pwd
+
+# user_list = [
+#  	User('id2', '1234'),
+#     User('id1', '2345')
+#     ]
+ 
+#  # iterable 객체이기 때문에 sorted 사용
+ 
+# result = sorted(user_list, key = attrgetter('id'))
+ 
+# for u in result:
+#     print(u.id)
+ 
+# # print(list(permutations(a, 3)))
+# # commit
+
+
+# dict = {1 : 1, 3 : 2, 2 : 2}
+# result = sorted(dict.items(), key=lambda a:(a[1], a[0]))
+# print(result)
+
+
+# A = [list() for _ in range(3)]
+# print(A)
+
+# new_A = [[1,2], [3, 4]]
+# A = new_A
+# print(A)
+
+
+
+a = [[0]* 2 for _ in range(3)]
 print(a)
 
+b = [[[0]* 2 for _ in range(4)] for k in range(3)]
 
-result = sorted(a, key= lambda a : (a[0], a[1]))
-print(result)
-
-class User:
-    
-    def __init__(self, id, pwd):
-        self.id = id
-        self.pwd = pwd
-
-user_list = [
- 	User('id2', '1234'),
-    User('id1', '2345')
-    ]
- 
- # iterable 객체이기 때문에 sorted 사용
- 
-result = sorted(user_list, key = attrgetter('id'))
- 
-for u in result:
-    print(u.id)
- 
-# print(list(permutations(a, 3)))
-# commit
-
-
-dict = {1 : 1, 3 : 2, 2 : 2}
-result = sorted(dict.items(), key=lambda a:(a[1], a[0]))
-print(result)
-
-
-A = [list() for _ in range(3)]
-print(A)
-
-new_A = [[1,2], [3, 4]]
-A = new_A
-print(A)
+print(b)
+b[2][0][1] = 1
+print("---")
+print(b)
+print(b[0])
