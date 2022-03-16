a0fb251baecac77b0c450377f95df8fc8c70fb4c
kim_ki_yun <kiyoon9390@gmail.com>
2022-03-16 16:28:29 +0900

    feat:dijkstra-problem

--- bj_9251.py
+++ bj_9251.py
@@ -2,5 +2,55 @@
 
 # dp[N] - N 번째까지 살펴봤을 때, 가장 긴 수열의 길이
 
-# dp[N] = dp[]
+# dp[N] = max(dp[N-1] + 만약 매치가 된다면 1 , dp[N])
 
+
+in1 = list(input().strip())
+in2 = list(input().strip())
+
+arr1 = ['0'] + in1
+arr2 = ['0'] + in2
+
+
+if len(arr1) > len(arr2):
+    long_arr = arr1
+    short_arr = arr2
+
+else:
+    long_arr = arr2
+    short_arr = arr1
+
+
+dp = [0]*len(short_arr)
+
+start = 1
+
+for i in range(1, len(short_arr)):
+    detect_flag = False
+    if start > len(long_arr)-1:
+        dp[i] = dp[i-1]
+        continue
+
+    for j in range(start, len(long_arr)):
+        if short_arr[i] == long_arr[j] :
+            dp[i] = dp[i-1] + 1
+            detect_flag = True
+            start = j+1
+            print(short_arr[i])
+            break
+
+    if detect_flag == False:
+        dp[i] = dp[i-1]
+
+
+print(dp[len(short_arr)-1])
+
+
+# AC 로 시작할 수도 CA로 시작할 수도
+
+
+# DP[i][j] i번쨰에서 j번째까지의 가장 LCS의 길이
+# dp[i][j] = max(dp[i][k] + dp[k+1][j]) i = k ~ j-1
+
+# 초기값 문제
+# dp[i][i] = 
---
+++ tc_다익스트라-1.py
@@ -0,0 +1,49 @@
+
+# 방문 판매원 A는 현재 1번 회사
+# X번 회사에 방문해 물건 판매
+
+# 2개의 회사는 양방향 이동 가능
+# 1만큼의 cost
+
+# 소개팅 상대 K번 회사 에 존재
+# 1번 -> K번 -> X번 회사
+
+# 플로이드 워셜을 이용해야!!!!
+
+N, M = map(int, input().split())
+
+dist = [[int(1e9)]* (N+1) for _ in range(N+1)] # dist - i, j 와의 최소 거리 
+
+for i in range(1, N+1):
+    for j in range(1, N+1):
+        if i==j:
+            dist[i][j] = 0
+
+for _ in range(M):
+    a, b = map(int, input().split())
+    dist[a][b] = 1
+    dist[b][a] = 1
+
+X, K = map(int, input().split())
+
+# dist[i][j] = min(dist[i][k] + dist[k][j]) k = i ~ j
+
+for k in range(1, N+1):
+    for i in range(1, N+1):
+        for j in range(1, N+1):
+            dist[i][j] = min(dist[i][j], dist[i][k] + dist[k][j])
+            
+
+
+if dist[1][K] + dist[K][X] >= int(1e9):
+    print(-1)
+else:
+    print(dist[1][K] + dist[K][X])
+
+
+
+
+
+
+
+
