8f8524d2dc82015b80a21e1ac4cc147fec94e454
kim_ki_yun <kiyoon9390@gmail.com>
2022-03-26 16:41:58 +0900

    feat:bj_5582

--- bj_5582.py
+++ bj_5582.py
@@ -1,4 +1,3 @@
-from itertools import permutations
 
 
 a = list(input())
@@ -12,8 +11,8 @@ dp = [[0]*len(b) for _ in range(len(a))]
 
 answer = 0
 
-for i in range(len(a)):
-    for j in range(len(b)):
+for i in range(1, len(a)):
+    for j in range(1, len(b)):
         if a[i] == b[j]:
             dp[i][j] = dp[i-1][j-1] + 1
             answer = max(answer, dp[i][j])
