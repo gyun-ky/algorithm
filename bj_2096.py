3170d5d3917bcf0afc32519b113eb7721195ed85
kim_ki_yun <kiyoon9390@gmail.com>
2022-05-08 23:56:12 +0900

    feat:bj_2096 - dp

---
+++ bj_2096.py
@@ -0,0 +1,74 @@
+# N줄에 0이상 9 이하의 숫자 세개씩
+# 첫줄에서 마지막 줄에서 끝나느 놀이
+
+# 첫줄 세개 숫자 중 한개 선택 -> 다음줄 (제약조건 : 바로 아래로 넘어가기 혹은 바로 아래수와 붙어있는 수로)
+# 얻을 수 있느 최대 점수, 최소 점수
+
+N = int(input())
+
+data = []
+for i in range(N):
+    data.append(list(map(int, input().split())))
+
+# 시간 복잡도
+# N - 100,000
+# 3 ^ 100,000 -> 최대, 최소를 구해야 하는데, 완전탐색 불가!!! 시간 초과난다
+
+# 이게 결국 3 * 100,000를 방문하는 방법을 찾는 것이다!!
+
+# 그리디가 아닌가
+# 작은것을 선택 -> 그다음에도 작은 것을 선택 
+# 그런데 이것은 현재, 123,921인 경우 3으로 가서 81을 선택하는것보다 1로가서 9를 선택하는게 좋으므로 -> 그리디 불가
+
+# 투포인터로 푼다?????
+# 투포인터의 대상을 쌓여가는 크기라고 하고 그것들을 1차원 배열을 대상으로 보자!!!
+# 최대 9이하니까 900,001 크기의 배열 
+
+
+# 1KB = 1000 = 1024byte
+# 1MB = 1000,000 = 1024KB
+# int = 4byte = 2^2
+# 2^20 / 2^2 = 2^18
+# 1000,000 * 4 / 4 = 1000,000 개의 숫자!!!
+
+max_num = 0
+min_num = int(1e9)
+
+# dp1[i][j] : i,j까지 왔을 때의 최소
+# dp1[i][j] = 
+## j가 0인 경우 : min(dp[i-1][0] + data[i][j], dp[i-1][1] + data[i][j])
+## j가 1인 경우 : min(dp[i-1][0] + data[i][j], dp[i-1][1] + data[i][j], dp[i-1][2] + data[i][j])
+## j가 2인 경우 : min(dp[i-1][1] + data[i][j], dp[i-1][2] + data[i][j])
+
+dp1 = [[int(1e9)] * 3 for _ in range(N)]
+dp2 = [[-1]*3 for _ in range(N)]
+for i in range(3):
+    dp1[0][i], dp2[0][i] = data[0][i], data[0][i]
+
+for i in range(1,N):
+    for j in range(3):
+        if j == 0:
+            tmp1 = [dp1[i][j], dp1[i-1][0] + data[i][j], dp1[i-1][1] + data[i][j]]
+            tmp2 = [dp2[i][j], dp2[i-1][0] + data[i][j], dp2[i-1][1] + data[i][j]]
+            
+        elif j == 1:
+            tmp1 = [dp1[i][j], dp1[i-1][0] + data[i][j], dp1[i-1][1] + data[i][j], dp1[i-1][2] + data[i][j]]
+            tmp2 = [dp2[i][j], dp2[i-1][0] + data[i][j], dp2[i-1][1] + data[i][j], dp2[i-1][2] + data[i][j]]
+    
+        else:
+            tmp1 = [dp1[i][j], dp1[i-1][1] + data[i][j], dp1[i-1][2] + data[i][j]]
+            tmp2 = [dp2[i][j], dp2[i-1][1] + data[i][j], dp2[i-1][2] + data[i][j]]
+    
+        dp1[i][j] = min(tmp1)
+        dp2[i][j] = max(tmp2)
+
+print(max(dp2[N-1]), end=' ')
+print(min(dp1[N-1]), end=' ')
+
+# 3*100,000 = 300,000 / 300,000 * 4 * 3 -> 3600,000
+# 4000,000
+
+
+# dp2[i][j] : i,j까지 왔을 떄의 최대
+
+
--- test1.py
+++ test1.py
@@ -1,43 +1,123 @@
-from itertools import permutations
-from copy import deepcopy
+from collections import deque
 
-weak_size = 0
+N = None
 
+dx = [0, 1, 0, -1]
+dy = [1, 0, -1, 0]
 
-def dfs(fidx, pos, processed, check_list, order):
-    
-    if fidx == len(order):
-        return int(1e9)
-    
-    
+visited = None
 
-
-def solution(n, weak, dist):
-    global weak_size
-
-    weak_size = len(weak)
-    n_weak = deepcopy(weak)
-    for w in weak:
-        n_weak.append(w+n)
-        
-    print(n_weak)
-    
-    answer = int(1e9)
-    
-    for start in range(0, weak_size):
-        
-        check_list = [n_weak[i] for i in range(start, start+weak_size)]
-        
-        for order in permutations(dist, len(dist)):
-            
-            answer = min(answer, dfs(0, check_list[0], 0, check_list, order))
-                
-    
-    if answer == int(1e9):
-        return -1
+def check_boundary(a, b, c, d):
+    # 이동한 위치가 지도 안에 있는지 확인
+    if 0<=a<N and 0<=b<N and 0<=c<N and 0<=d<N:
+        return True
     else:
-        return answer
+        return False
 
+def change_rot(shape):
+    # 만약 로테이션이면 모양을 바꾸어준다
+    if shape == 'v':
+        return 'h'
+    else:
+        return 'v'
+
+def check_visited(a, b, c, d, shape):
+    global visited
     
-    
+    if shape == 'h':
+        s = 0
+    else:
+        s = 1
         
+    if visited[s][a][b] == False and visited[s][c][d] == False:
+        # 방문할 것이므로 visited 처리까지 한다
+        visited[s][a][b] = True 
+        visited[s][c][d] = True
+        return True
+    else:
+        return False
+        
+        
+    
+
+def rotation(rot_num, board, a, b, c, d, shape):
+    # 회전 가능 
+    # (a,b)(c,d) -> (a+1,b+1)(c,d) 조건 (a+1,b)이 1이 아니어야
+    
+    ns = change_rot(shape)
+    
+    if rot_num == 0:
+        na, nb = a+1, b+1
+        if check_boundary(na, nb, c, d):
+            if board[a+1][b] != 1 and check_visited(na, nb, c, d, ns):
+                return (True, na, nb, c, d, ns)
+            else:
+                return (False, a, b, c, d, shape)
+    # (a,b)(c,d) -> (a-1,b+1)(c,d) 조건 (a-1,b)가 1이 아니어야
+    if rot_num == 1:
+        na, nb = a-1, b+1
+        if check_boundary(na, nb, c, d) and check_visited(na, nb, c, d, ns):
+            if board[a-1][b] != 1:
+                return (True, na, nb, c, d, ns)
+            else:
+                return (False, a, b, c, d, shape)
+    # (a,b)(c,d) -> (a,b)(c+1,d-1) 조건 (c+1,d)가 1이 아니어야
+    if rot_num == 2:
+        nc, nd = c+1, d-1
+        if check_boundary(a, b, nc, nd) and check_visited(a, b, nc, nd, ns):
+            if board[c+1][d] != 1:
+                return (True, a, b, nc, nd, ns)
+            else:
+                return (False, a, b, c, d, shape)
+    # (a,b)(c,d) -> (a,b)(c-1,d-1) 조건 (c-1,d)가 1이 아니어야
+    if rot_num == 3:
+        nc, nd = c-1, d-1
+        if check_boundary(a, b, nc, nd) and check_visited(a, b, nc, nd, ns):
+            if board[c-1][d] != 1:
+                return (True, a, b, nc, nd, ns)
+            else:
+                return (False, a, b, c, d, shape)
+            
+def bfs(board, a, b, c, d, shape):
+    
+    q = deque()
+    q.append([a, b, c, d, 'h', 0])
+    visited[0][a][b] = True
+    visited[0][c][d] = True
+    
+    while q:
+        ca, cb, cc, cd, s, cnt = q.popleft()
+        
+        if (ca==N-1 and cb==N-1) or (cc == N-1 and cd == N-1):
+            return cnt
+        
+        for d in range(4):
+            na, nb, nc, nd = ca + dx[d], cb + dy[d], cc + dx[d], cd + dy[d]
+            
+            if check_boundary(na, nb, nc, nd):
+                if check_visited(na, nb, nc, nd, s):
+                    q.append([na, nb, nc, nd, s, cnt+1])
+            
+            
+            go, na, nb, nc, nd, ns = rotation(d, board, ca, cb, cc, cd, s)
+            if go == True:
+                q.append([na, nb, nc, nd, ns, cnt+1])
+    
+    
+
+def solution(board):
+    global N, visited
+    
+    N = len(board[0])
+    # visited[0][i][j] - 로봇이 가로로 놓여있을 때 방문 여부 체크
+    # visited[1][i][j] - 로봇이 세로로 놓여있을 때 방문 여부 체크
+    visited = [[[False]*N for i in range(N)] for k in range(2)]
+    
+    answer = bfs(board, 0, 0, 0, 1, 'h')
+    # N*N 크기 지도 / 1*2 로봇
+    # (0,0)(0,1) 부터 시작 -> 0 - 빈칸 / 1 - 벽
+    
+    return answer
+
+
+print(solution([[0, 0, 0, 1, 1],[0, 0, 0, 1, 0],[0, 1, 0, 1, 1],[1, 1, 0, 0, 1],[0, 0, 0, 0, 0]]))
